5ee4b8f8044f84ec6d2c93235ca7bb3d3ca77cfa
Beth Tenorio <betsky98@gmail.com>
2013-11-22 23:21:17 -0500

    Add JS script for transcribing dna

---
+++ transcribing_dna.js
@@ -0,0 +1,6 @@
+
+  tag = "Transcribed Sequence: "
+  var sequence = document.getElementById("sequence").innerHTML
+  var transcribed_sequence = sequence.replace('T', 'U')
+  // document.writeln("<p>" + tag + "</p>")
+  // document.write("<p>" + transcribed_sequence + "</p>")
